068e7f27e8e985f256b5768021f33a0072d17bd6
EpicWolf <a.ladaev08@mail.ru>
2026-02-08 13:21:05 +0300

    Added compatibility for new custom textures mechanic and deleted redundant methods

--- src/client/java/com/epicwolf/epicsorigins/client/EpicsoriginsClient.java
+++ src/client/java/com/epicwolf/epicsorigins/client/EpicsoriginsClient.java
@@ -5,15 +5,21 @@ import com.epicwolf.epicsorigins.client.command.Commands;
 import com.epicwolf.epicsorigins.client.networking.ModPacketsS2C;
 import com.epicwolf.epicsorigins.client.render.feature.PlayerLookFeatureRenderer;
 import com.epicwolf.epicsorigins.client.render.model.PlayerLookModel;
+import com.epicwolf.epicsorigins.networking.ModPackets;
 import io.github.apace100.apoli.ApoliClient;
+import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.HashMap;
@@ -25,7 +31,7 @@ public class EpicsoriginsClient implements ClientModInitializer {
     public static final EntityModelLayer PLAYER_ATTRIBUTES = new EntityModelLayer(
             Epicsorigins.identifier("player_attributes"), "main");
 
-    public static final Map<UUID, PlayerTextureData> CLIENT_PLAYER_TEXTURES = new HashMap<>();
+    public static final Map<UUID, Identifier> PLAYER_TEXTURES = new HashMap<>();
 
     public static KeyBinding useChangeLookPower;
 
@@ -38,6 +44,11 @@ public class EpicsoriginsClient implements ClientModInitializer {
                 registrationHelper.register(new PlayerLookFeatureRenderer<>(playerEntityRenderer, context));
             }
         });
+        ClientPlayConnectionEvents.JOIN.register((handler,  sender, minecraftClient) -> {
+            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer().writeBoolean(false));
+            buf.writeString("");
+            ClientPlayNetworking.send(ModPackets.SEND_PLAYER_LOOK_TEXTURE, buf);
+        });
 
         Commands.register();
         ModPacketsS2C.register();
@@ -47,66 +58,5 @@ public class EpicsoriginsClient implements ClientModInitializer {
         ApoliClient.registerPowerKeybinding("changelook", useChangeLookPower);
 
         KeyBindingHelper.registerKeyBinding(useChangeLookPower);
-
-        // Регистрация пакетов
-//        registerClientPackets();
-
-        // Регистрация слушателя ресурсов
-//        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
-//                .registerReloadListener(new PlayerTextureReloadListener());
-
-//        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-//            if (client.world != null && client.player != null) {
-//                requestPlayerTextures();
-//            }
-//        });
-    }
-
-    /*TODO*/
-    private void registerClientPackets() {
-//        ClientPlayNetworking.registerGlobalReceiver(
-//                Epicsorigins.identifier("player_texture_data"),
-//                (client, handler, buf, responseSender) -> {
-//                    UUID playerUuid = buf.readUuid();
-//                    String textureData = buf.readString();
-//                    boolean hasWings = buf.readBoolean();
-//
-//                    client.execute(() -> {
-//                        CLIENT_PLAYER_TEXTURES.put(playerUuid,
-//                                new PlayerTextureData(textureData, hasWings));
-//                        PlayerTextureGenerator.generatePlayerTexture(playerUuid, textureData);
-//                    });
-//                }
-//        );
-    }
-
-    /*TODO*/
-    private void requestPlayerTextures() {
-//        MinecraftClient client = MinecraftClient.getInstance();
-//        if (client.world != null) {
-//            client.world.getPlayers().forEach(player -> {
-//                if (!player.getUuid().equals(client.player.getUuid()) &&
-//                        !CLIENT_PLAYER_TEXTURES.containsKey(player.getUuid())) {
-//
-//                    // Запрашиваем данные игрока
-//                    PacketByteBuf buf = PacketByteBufs.create();
-//                    buf.writeUuid(player.getUuid());
-//                    ClientPlayNetworking.send(
-//                            Epicsorigins.identifier("request_player_data"), buf);
-//                }
-//            });
-//        }
-    }
-
-
-    /*TODO*/
-    public static class PlayerTextureData {
-//        public String textureData;
-//        public boolean hasWings;
-//
-//        public PlayerTextureData(String textureData, boolean hasWings) {
-//            this.textureData = textureData;
-//            this.hasWings = hasWings;
-//        }
     }
 }
--- src/main/java/com/epicwolf/epicsorigins/Epicsorigins.java
+++ src/main/java/com/epicwolf/epicsorigins/Epicsorigins.java
@@ -5,7 +5,10 @@ import com.epicwolf.epicsorigins.item.ModItems;
 import com.epicwolf.epicsorigins.networking.ModPacketsC2S;
 import com.epicwolf.epicsorigins.power.factory.PowerFactories;
 import com.epicwolf.epicsorigins.power.factory.action.EntityActions;
+import com.epicwolf.epicsorigins.util.ModConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
 import org.apache.logging.log4j.Logger;
@@ -17,9 +20,31 @@ public class Epicsorigins implements ModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger(Epicsorigins.class);
 
+    public static ModConfig config;
+
+    public static String VERSION = "";
+
     @Override
     public void onInitialize() {
-//        registerPackets();
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent((modContainer) -> {
+            VERSION = modContainer.getMetadata().getVersion().getFriendlyString();
+            if (VERSION.contains("+")) {
+                VERSION = VERSION.split("\\+")[0];
+            }
+
+            if (VERSION.contains("-")) {
+                VERSION = VERSION.split("-")[0];
+            }
+
+        });
+        LOGGER.info("Epic`s Origins {} is initializing", VERSION);
+        config = new ModConfig();
+        config.modConfigDir.mkdirs();
+
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            Epicsorigins.config.loadTexturesToPlayer(handler.getPlayer());
+        });
+
         ModPacketsC2S.register();
         PowerFactories.register();
         EntityActions.register();
@@ -29,30 +54,5 @@ public class Epicsorigins implements ModInitializer {
 
     public static Identifier identifier(String id) {
         return Identifier.of(MOD_ID, id);
-    }
-
-    /*TODO*/
-    private void registerPackets() {
-//        ServerPlayNetworking.registerGlobalReceiver(
-//                new Identifier(MOD_ID, "update_player_texture"),
-//                (server, player, handler, buf, responseSender) -> {
-//                    String textureData = buf.readString();
-//                    boolean hasWings = buf.readBoolean();
-//                    server.execute(() -> {
-//                        PlayerTextureManager.savePlayerTexture(player.getUuid(), textureData, hasWings);
-//                        PlayerTextureManager.syncPlayerData(player);
-//                    });
-//                }
-//        );
-//
-//        ServerPlayNetworking.registerGlobalReceiver(
-//                new Identifier(MOD_ID, "request_player_data"),
-//                (server, player, handler, buf, responseSender) -> {
-//                    UUID targetUuid = buf.readUuid();
-//                    server.execute(() -> {
-//                        PlayerTextureManager.sendPlayerDataToClient(player, targetUuid);
-//                    });
-//                }
-//        );
     }
 }
