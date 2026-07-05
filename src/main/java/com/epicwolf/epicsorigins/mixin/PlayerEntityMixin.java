ea01d10cfb6ba6c98cec3b5df1c608903a24c2fd
EpicSurg <a.ladaev08@mail.ru>
2026-07-05 17:22:27 +0300

    Added hitbox changes with size power

--- src/main/java/com/epicwolf/epicsorigins/mixin/PlayerEntityMixin.java
+++ src/main/java/com/epicwolf/epicsorigins/mixin/PlayerEntityMixin.java
@@ -1,6 +1,7 @@
 package com.epicwolf.epicsorigins.mixin;
 
 import com.epicwolf.epicsorigins.power.EntityTransformationPower;
+import com.epicwolf.epicsorigins.power.SizePower;
 import dev.onyxstudios.cca.api.v3.component.ComponentProvider;
 import io.github.apace100.apoli.access.MovingEntity;
 import io.github.apace100.apoli.access.SubmergableEntity;
@@ -15,16 +16,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
 import java.util.Objects;
 
-@Mixin(PlayerEntity.class)
+@Mixin(value = PlayerEntity.class, priority = 900)
 public abstract class PlayerEntityMixin extends Entity implements MovingEntity, SubmergableEntity, ComponentProvider {
 
     @Unique
-    private boolean isInitialized;
-
-    @Unique
-    private boolean hadTransformPower;
+    private boolean isChangedDimensions;
 
     public PlayerEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -36,56 +35,78 @@ public abstract class PlayerEntityMixin extends Entity implements MovingEntity, 
             EntityTransformationPower power = PowerHolderComponent.getPowers(this, EntityTransformationPower.class).get(0);
             if (power.isTransformed()) {
                 EntityDimensions dimensions = EntityType.get(power.getEntityType().toString()).get().getDimensions();
+                if (List.of(EntityPose.FALL_FLYING, EntityPose.SWIMMING, EntityPose.SPIN_ATTACK).contains(Objects.requireNonNull(pose)) && dimensions.height > dimensions.width) {
+                    cir.setReturnValue(new EntityDimensions(dimensions.width, dimensions.width, dimensions.fixed));
+                }
                 if (Objects.requireNonNull(pose) == EntityPose.CROUCHING) {
-                    cir.setReturnValue(new EntityDimensions(dimensions.width, (float) (dimensions.height * 0.8), false));
+                    cir.setReturnValue(new EntityDimensions(dimensions.width, (float) (dimensions.height * 0.83), dimensions.fixed));
                 } else {
                     cir.setReturnValue(dimensions);
                 }
             }
         }
     }
+    @Inject(method = "getDimensions", at = @At("RETURN"), cancellable = true)
+    private void modifyDimensions(EntityPose pose, CallbackInfoReturnable<EntityDimensions> cir) {
+        if (PowerHolderComponent.hasPower(this, SizePower.class)) {
+            EntityDimensions dimensions = cir.getReturnValue();
+            SizePower power = PowerHolderComponent.getPowers(this, SizePower.class).get(0);
+            float width = power.getWidth() <= 0 ? dimensions.width : power.getWidth();
+            float height = power.getHeight() <= 0 ? dimensions.height : power.getHeight();
+            float size = power.getSize();
+            cir.setReturnValue(new EntityDimensions(width*size, height*size, dimensions.fixed));
+        }
+    }
 
     @Inject(method = "getActiveEyeHeight", at = @At("HEAD"), cancellable = true)
     public void TransformedEyeHeight(EntityPose pose, EntityDimensions dimensions, CallbackInfoReturnable<Float> cir) {
-        if (isInitialized) {
+        try {
             if (PowerHolderComponent.hasPower(this, EntityTransformationPower.class)) {
                 EntityTransformationPower power = PowerHolderComponent.getPowers(this, EntityTransformationPower.class).get(0);
                 if (power.isTransformed()) {
-                    float height = EntityType.get(power.getEntityType().toString()).get().getDimensions().height;
+                    float height = dimensions.height;
                     if (power.isTransformed()) {
-                        switch (pose) {
-                            case SWIMMING:
-                            case FALL_FLYING:
-                            case SPIN_ATTACK:
-                                cir.setReturnValue(height * 0.4F);
-                                return;
-                            case CROUCHING:
-                                cir.setReturnValue(height * 0.65F);
-                                return;
-                            default:
-                                cir.setReturnValue(height * 0.85F);
-                        }
+                        cir.setReturnValue(applyEyeHeight(pose, height));
                     }
                 }
             }
-        }
-        else {
-            isInitialized = true;
-        }
+        } catch (Exception ignored) {}
+    }
+    @Inject(method = "getActiveEyeHeight", at = @At("RETURN"), cancellable = true)
+    private void modifyEyeHeight(EntityPose pose, EntityDimensions dimensions, CallbackInfoReturnable<Float> cir) {
+        try {
+            if (PowerHolderComponent.hasPower(this, SizePower.class)) {
+                cir.setReturnValue(applyEyeHeight(pose, dimensions.height));
+            }
+        } catch (Exception ignored) {}
+    }
+
+    @Unique
+    private float applyEyeHeight(EntityPose pose, float height) {
+        return switch (pose) {
+            case SWIMMING, FALL_FLYING, SPIN_ATTACK -> height * 2 / 3;
+            case CROUCHING -> height * 0.846F;
+            default -> height * 0.9F;
+        };
     }
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void tick(CallbackInfo ci) {
-        if (PowerHolderComponent.hasPower(this, EntityTransformationPower.class)) {
-            EntityTransformationPower power = PowerHolderComponent.getPowers(this, EntityTransformationPower.class).get(0);
-            if (power.isTransformed() != power.wasTransformed()) {
-                power.setWasTransformed(power.isTransformed());
-                this.calculateDimensions();
+        if (PowerHolderComponent.hasPower(this, EntityTransformationPower.class) || PowerHolderComponent.hasPower(this, SizePower.class)) {
+            if (PowerHolderComponent.hasPower(this, EntityTransformationPower.class)) {
+                EntityTransformationPower power = PowerHolderComponent.getPowers(this, EntityTransformationPower.class).get(0);
+                if (power.isTransformed() != power.wasTransformed()) {
+                    power.setWasTransformed(power.isTransformed());
+                    isChangedDimensions = false;
+                }
             }
-            this.hadTransformPower = true;
+            if (!isChangedDimensions) {
+                this.calculateDimensions();
+                this.isChangedDimensions = true;
+            }
         }
-        else if (hadTransformPower) {
-            this.hadTransformPower = false;
+        else if (isChangedDimensions) {
+            this.isChangedDimensions = false;
             this.calculateDimensions();
         }
     }
